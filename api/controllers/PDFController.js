363f6e2f0d496f1c96bd2520acc9496069443534
Harit Joshi <joshiharit3@gmail.com>
2021-08-19 09:25:57 +0530

    pdf updates old

--- api/controllers/PDFController.js
+++ api/controllers/PDFController.js
@@ -1,3 +1,10 @@
+const axios = require("axios").default;
+const path = require("path");
+const fs = require("fs");
+
+const docxConverter = require("docx-pdf");
+
+const { PDFNet } = require("@pdftron/pdfnet-node");
 const catchAsync = require("../utils/catchAsync");
 
 exports.getData = catchAsync(async (req, res) => {
@@ -9,4 +16,21 @@ exports.getData = catchAsync(async (req, res) => {
 
 exports.convert = catchAsync(async (req, res) => {
   const { filename } = req.query;
+  const outputFile = String(filename).split(".docx")[0];
+
+  const inputPath = path.join(__dirname, `../files/${filename}`);
+  const outputPath = path.join(__dirname, `../output/${outputFile}.pdf`);
+
+  docxConverter(inputPath, outputPath, (err, obj) => {
+    if(err){
+      res.status(500).json({
+        status: "failed",
+        message: "Something went wrong."
+      })
+    }
+    const file = fs.readFileSync(obj.filename);
+    res.setHeader('Content-Type', 'application/pdf').status(200).end(file);
+  });
+
 });
+
